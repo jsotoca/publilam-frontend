1a2d7ddcabdebbee27e65cf9a501d91a740b967f
jsotoca <jsotoca@unprg.edu.pe>
2021-02-13 13:18:37 -0500

    Add Leaflet map in search page

--- src/app/pages/business/business.page.ts
+++ src/app/pages/business/business.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Map,tileLayer,marker} from 'leaflet';
 @Component({
   selector: 'app-business',
   templateUrl: './business.page.html',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessPage implements OnInit {
 
+  map:Map;
+  newMarker:any;
+  address:string[];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter(){
+    if(!this.map) this.loadMap();
+  }
+
+  loadMap(){
+    this.map = new Map("businessMap").setView([-6.7662462,-79.8435701], 16);
+    tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      { attribution: `Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`}
+    )
+    .addTo(this.map);
   }
 
 }
