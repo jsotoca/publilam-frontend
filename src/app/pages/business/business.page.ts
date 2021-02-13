19e68a26337894bcc16d0e9e90625e04a181e58c
jsotoca <jsotoca@unprg.edu.pe>
2021-02-13 13:32:57 -0500

    Disable scroll whell zoom in map

--- src/app/pages/business/business.page.ts
+++ src/app/pages/business/business.page.ts
@@ -22,6 +22,7 @@ export class BusinessPage implements OnInit {
 
   loadMap(){
     this.map = new Map("businessMap").setView([-6.7662462,-79.8435701], 16);
+    this.map.scrollWheelZoom.disable();
     tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       { attribution: `Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`}
